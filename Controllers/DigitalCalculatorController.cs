ac4180350cda7bfea13fd6c71fab44233c58e82d
parth <parth@PARTH14>
2024-10-08 16:18:31 -0400

    Add project files.

---
+++ Controllers/DigitalCalculatorController.cs
@@ -0,0 +1,48 @@
+﻿using DigitalCalculatorAPI.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalCalculatorAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DigitalCalculatorController : ControllerBase
+    {
+        private readonly ICalculatorService _calculatorService;
+
+        public DigitalCalculatorController(ICalculatorService calculatorService)
+        {
+            _calculatorService = calculatorService;
+        }
+
+        [HttpGet("add")]
+        public ActionResult<double> Add(double a, double b)
+        {
+            return _calculatorService.Add(a, b);
+        }
+
+        [HttpGet("subtract")]
+        public ActionResult<double> Subtract(double a, double b)
+        {
+            return _calculatorService.Subtract(a, b);
+        }
+
+        [HttpGet("multiply")]
+        public ActionResult<double> Multiply(double a, double b)
+        {
+            return _calculatorService.Multiply(a, b);
+        }
+
+        [HttpGet("divide")]
+        public ActionResult<double> Divide(double a, double b)
+        {
+            try
+            {
+                return _calculatorService.Divide(a, b);
+            }
+            catch (DivideByZeroException)
+            {
+                return BadRequest("Division by zero is not allowed.");
+            }
+        }
+    }
+}
---
+++ Services/CalculatorService.cs
@@ -0,0 +1,19 @@
+﻿using DigitalCalculatorAPI.Interfaces;
+
+namespace DigitalCalculatorAPI.Services
+{
+    public class CalculatorService : ICalculatorService
+    {
+        public double Add(double a, double b) => a + b;
+
+        public double Subtract(double a, double b) => a - b;
+
+        public double Multiply(double a, double b) => a * b;
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0) throw new DivideByZeroException();
+            return a / b;
+        }
+    }
+}
